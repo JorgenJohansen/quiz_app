0812f97ed127e7f864479c661c7449d8d7facb97
torniklas <torniklas@outlook.com>
2020-03-09 13:04:30 +0100

    made skeleton

---
+++ app.py
@@ -0,0 +1,30 @@
+
+# Import questions, return JSON object with 
+# questions, answers, and more reading material
+def import_questions():
+    return None
+
+# Evaluates a question, returns True if correct answer, False otherwise
+def evaluate_question(question, answer):
+    return None
+
+# Presents a question to the user with the help of the UI
+def present_question(question):
+    return None
+
+# Gives feedback to the user with the help of the UI
+def give_feedback():
+    return None
+
+# Initiate the UI
+def init_ui():
+    return None
+
+# Main function
+def main():
+    return None
+
+
+
+if __name__== "__main__":
+  main()
